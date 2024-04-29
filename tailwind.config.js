d43475ebfb1be06af7eae3e181433def22ad6895
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-04-29 11:25:36 +0200

    login form css improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {"400":"#3984ff", "700":"#0057e5"},
-        background: {"200":"#e3e3e3"}
-      }
+        primary: { 400: "#000000", 700: "#454747" },
+        background: { 200: "#e3e3e3" },
+      },
     },
   },
   plugins: [],
